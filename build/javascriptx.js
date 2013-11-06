d9305b70e555d659fa6cced46f1c6b02ce3dfda2
BlackPrincess <black.princess.w@gmail.com>
2013-11-06 12:24:03 +0900

    take a cup coffee

--- build/javascriptx.js
+++ build/javascriptx.js
@@ -640,83 +640,6 @@ if (!Array.prototype.some) {
     return false;
   };
 }
-Array.prototype.sum = function() {
-  if(this.length === 0) {
-    return 0;
-  } else if (this.length === 1){
-    return parseInt(this[0], 10);
-  }
-  return this.reduce(function(x, y) { return x + y; });
-};
-
-Array.prototype.head = function() {
-  if(this.length === 0 ) throw "NoSuchElement";
-  return this[0];
-};
-
-Array.prototype.headOption = function() {
-  return Option.apply(this[0]);
-};
-
-Array.prototype.last = function() {
-  if(this.length === 0 ) throw "NoSuchElement";
-  return this[this.length - 1];
-};
-
-Array.prototype.lastOption = function() {
-  return Option.apply(this[this.length -1 ]);
-};
-
-Array.prototype.get = function(i) {
-  return this[i];
-};
-
-Array.prototype.getOrElse = function(i, or) {
-  return this[i] !== undefined ? this[i] : or;
-};
-
-
-/**
- * Returns a sequence formed from this sequence and another sequence 
- * by combining corresponding elements in pairs
- * if the two sequence is not same length, the length of returning sequence is same short one's.
- * @param {Array} arr
- * @return {Array} combined corresponding elements in pairs
- */
-Array.prototype.zip = function(arr) {
-  var ret = [];
-  var base = (this.length < arr.length) ? this : arr;
-  for(var i = 0, l = base.length; i < l; ++i) {
-    ret[i] = [this[i], arr[i]];
-  }
-  return ret;
-};
-
-/**
- * Returns a sequence formed from this sequence and another sequence 
- * by combining corresponding elements in pairs
- * if the two sequence is not same length, 
- * this sequence will be padded with defaultA or another sequence will be padded with defaultB.
- * @param {Array} arr
- * @param {Object} defaultA 
- * @param {Object} defaultB
- * @return {Array} combined corresponding elements in pairs.
- */
-Array.prototype.zipAll = function(arr, defaultA, defaultB) {
-  var ret = [];
-  var base = (this.length > arr.length) ? this : arr;
-  for(var i = 0, l = base.length; i < l; ++i) {
-    var a = (this[i] === undefined) ? defaultA : this[i];
-    var b = (arr[i] === undefined) ? defaultB : arr[i];
-    ret[i] = [a, b];
-  }
-  return ret;
-};
-
-Array.prototype.clone = function() {
-  return Array.apply(null, this);
-};
-
 (function() {
   /**
    * 該当付きに何日あるかを返します
@@ -1210,6 +1133,106 @@ String.prototype.removeFullWidth = function() {
 };
 
 
+
+(function() {
+  Array.prototype.sum = function() {
+    if (this.length === 0) {
+      return 0;
+    } else if (this.length === 1) {
+      return parseInt(this[0], 10);
+    } else {
+      return this.reduce(function(x, y) {
+        return x + y;
+      });
+    }
+  };
+
+  Array.prototype.head = function() {
+    if (this.length === 0) {
+      throw "NoSuchElement";
+    } else {
+      return this[0];
+    }
+  };
+
+  Array.prototype.headOption = function() {
+    return Option.apply(this[0]);
+  };
+
+  Array.prototype.last = function() {
+    if (this.length === 0) {
+      throw "NoSuchElement";
+    } else {
+      return this[this.length - 1];
+    }
+  };
+
+  Array.prototype.lastOption = function() {
+    return Option.apply(this[this.length - 1]);
+  };
+
+  Array.prototype.get = function(i) {
+    return this[i];
+  };
+
+  Array.prototype.getOrElse = function(i, orV) {
+    if (this[i] !== void 0) {
+      return this[i];
+    } else {
+      return orV;
+    }
+  };
+
+  /*
+  # Returns a sequence formed from this sequence and another sequence 
+  # by combining corresponding elements in pairs
+  # if the two sequence is not same length, the length of returning sequence is same short one's.
+  # @param {Array} arr
+  # @return {Array} combined corresponding elements in pairs
+  */
+
+
+  Array.prototype.zip = function(arr) {
+    var base, _i, _ref, _results;
+    base = this.length < arr.length ? this : arr;
+    return (function() {
+      _results = [];
+      for (var _i = 0, _ref = base.length; 0 <= _ref ? _i < _ref : _i > _ref; 0 <= _ref ? _i++ : _i--){ _results.push(_i); }
+      return _results;
+    }).apply(this).map(function(i) {
+      return [this[i], arr[i]];
+    }, this);
+  };
+
+  /*
+  # Returns a sequence formed from this sequence and another sequence 
+  # by combining corresponding elements in pairs
+  # if the two sequence is not same length, 
+  # this sequence will be padded with defaultA or another sequence will be padded with defaultB.
+  # @param {Array} arr
+  # @param {Object} defaultA 
+  # @param {Object} defaultB
+  # @return {Array} combined corresponding elements in pairs.
+  */
+
+
+  Array.prototype.zipAll = function(arr, defaultA, defaultB) {
+    var base, _i, _ref, _results;
+    base = this.length > arr.length ? this : arr;
+    return (function() {
+      _results = [];
+      for (var _i = 0, _ref = base.length; 0 <= _ref ? _i < _ref : _i > _ref; 0 <= _ref ? _i++ : _i--){ _results.push(_i); }
+      return _results;
+    }).apply(this).map(function(i) {
+      return [this[i] === void 0 ? defaultA : this[i], arr[i] === void 0 ? defaultB : arr[i]];
+    }, this);
+  };
+
+  Array.prototype.clone = function() {
+    return Array.apply(null, this);
+  };
+
+}).call(this);
 
 (function() {
   Boolean.unless = function(bool) {
---
+++ src/prototype/Array.prototype.coffee
@@ -0,0 +1,73 @@
+Array::sum = () ->
+  if this.length == 0
+    0
+  else if this.length == 1
+    parseInt(this[0], 10)
+  else
+    this.reduce (x, y) ->
+      x + y
+
+Array::head = () ->
+  if this.length == 0
+    throw "NoSuchElement"
+  else
+    this[0]
+
+Array::headOption = () ->
+  Option.apply(this[0])
+
+Array::last = () ->
+  if this.length == 0
+    throw "NoSuchElement"
+  else
+    this[this.length - 1]
+
+Array::lastOption = () ->
+  Option.apply(this[this.length - 1])
+
+
+Array::get = (i) ->
+  this[i]
+
+Array::getOrElse = (i, orV) ->
+  if this[i] != undefined
+    this[i] 
+  else 
+    orV
+
+###
+# Returns a sequence formed from this sequence and another sequence 
+# by combining corresponding elements in pairs
+# if the two sequence is not same length, the length of returning sequence is same short one's.
+# @param {Array} arr
+# @return {Array} combined corresponding elements in pairs
+###
+Array::zip = (arr) ->
+  base = if (this.length < arr.length) then this else arr
+  [0...base.length].map((i) ->
+    [this[i], arr[i]]
+  , this)
+
+
+###
+# Returns a sequence formed from this sequence and another sequence 
+# by combining corresponding elements in pairs
+# if the two sequence is not same length, 
+# this sequence will be padded with defaultA or another sequence will be padded with defaultB.
+# @param {Array} arr
+# @param {Object} defaultA 
+# @param {Object} defaultB
+# @return {Array} combined corresponding elements in pairs.
+###
+Array::zipAll = (arr, defaultA, defaultB) ->
+  base = if (this.length > arr.length) then this else arr
+  [0...base.length].map((i) ->
+    [
+      if this[i] == undefined then defaultA else this[i], 
+      if arr[i] == undefined then defaultB else arr[i]
+    ]
+  , this)
+
+Array::clone = () ->
+  Array.apply(null, this)
+
